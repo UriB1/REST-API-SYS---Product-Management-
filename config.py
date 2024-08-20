34316c3f1a6af9ef7e607bd822d89f4c3995b68e
UriB1 <106737885+UriB1@users.noreply.github.com>
2024-08-21 00:23:28 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,12 +2,16 @@ import os
 from dotenv import load_dotenv
 
 # Load environment variables from the .env file
+# This allows us to use environment-specific settings without hardcoding them into the source code.
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'your_secret_key')
-    FIREBASE_API_KEY = os.environ.get('FIREBASE_API_KEY', 'your_firebase_api_key')
-    FIREBASE_AUTH_DOMAIN = os.environ.get('FIREBASE_AUTH_DOMAIN', 'your_project_id.firebaseapp.com')
-    FIREBASE_DB_URL = os.environ.get('FIREBASE_DB_URL', 'https://your_project_id.firebaseio.com')
-    FIREBASE_STORAGE_BUCKET = os.environ.get('FIREBASE_STORAGE_BUCKET', 'your_project_id.appspot.com')
-    FIREBASE_SERVICE_ACCOUNT_KEY = os.environ.get('FIREBASE_SERVICE_ACCOUNT_KEY', 'path/to/serviceAccountKey.json')
+    # Secret key for session management and cryptographic operations
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'your_secret_key')  # Default value used if the environment variable is not set
+    
+    # Firebase configuration
+    FIREBASE_API_KEY = os.environ.get('FIREBASE_API_KEY', 'your_firebase_api_key')  # API key for Firebase authentication
+    FIREBASE_AUTH_DOMAIN = os.environ.get('FIREBASE_AUTH_DOMAIN', 'your_project_id.firebaseapp.com')  # Firebase authentication domain
+    FIREBASE_DB_URL = os.environ.get('FIREBASE_DB_URL', 'https://your_project_id.firebaseio.com')  # URL of the Firebase Realtime Database
+    FIREBASE_STORAGE_BUCKET = os.environ.get('FIREBASE_STORAGE_BUCKET', 'your_project_id.appspot.com')  # URL of the Firebase Storage bucket
+    FIREBASE_SERVICE_ACCOUNT_KEY = os.environ.get('FIREBASE_SERVICE_ACCOUNT_KEY', 'path/to/serviceAccountKey.json')  # Path to the Firebase service account key file
